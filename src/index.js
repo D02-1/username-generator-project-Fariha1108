8f497054bbfeb10328764aeea8f1e5ebecfec125
Fariha1108 <fariha_1990@outlook.de>
2021-09-23 11:03:31 +0200

    Add creatRandomNumber(x, y); function

--- src/index.js
+++ src/index.js
@@ -7,16 +7,31 @@ const path = require('path'); // Path, zum arbeiten mit dateipfaden
 
 // Was benötigen wir?
 // - Eine Funktion um die werte aus der JSON datei zu lesen
+/**
+ * @function getWords
+ * @description Reads words from json file.
+ * @returns { object }
+ */
 function getWords()
 {
+    // Wir lesen die json datei, die worte enthält, die wir nutzen wollen
     const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'words.json'), 'utf-8'));
 
+    // Wir geben die worte zurück
     return json;
 }
 
-getWords();
-
 // - Eine Funktion um die zufällige Nummer hinter dem Namen zu generieren
+function creatRandomNumber(minNumber, maxNumber)
+{
+    // Wir erschaffen eine zufallszahl aus einer minimalen und einer maximalen zahl
+    const num = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+
+    console.log(num);
+}
+
+creatRandomNumber(10000, 50000);
+
 // - Eine Funktion mit der wir den ersten Buchstaben des jeweiligen Wortes groß schreiben (Pascalcase)
 // - Eine Funktion mit der wir den benutzernamen generieren und auf die ergebnisse der anderen funktionen zugreifen
 // - Einen export, um die Applikation in einer anderen datei zu nutzen
